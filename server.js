0836832fb274bd123525077d24d14aaca22c9fd3
Nirmal Tamang <lamanirmal27@gmail.com>
2025-11-04 05:56:00 +0000

    server.js update

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
  
 app.get("/", (req, res) => {
-  res.send("Hello from Node.js Sample App 🚀");
+  res.send("Hello from Node.js Sample App 🚀 updated!!");
 });
  
 app.get("/health", (req, res) => {
